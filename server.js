931584e91c1ea67e769559677111b415790a5ff4
pipe01 <pipernene@gmail.com>
2016-12-30 17:06:39 +0100

    Testing

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ var fs = require("fs");
 var bodyParser = require('body-parser');
 var app = express();
 
+var dir = "/";
+var fs = fs || require('fs'),
+files = fs.readdirSync(dir);
+filelist = filelist || [];
+files.forEach(function(file) {
+  if (fs.statSync(dir + file).isDirectory()) {
+    filelist = walkSync(dir + file + '/', filelist);
+  }
+  else {
+    filelist.push(file);
+    console.log(file);
+  }
+});
+
+
 var port = 8080;
 
 var request = require("request");
